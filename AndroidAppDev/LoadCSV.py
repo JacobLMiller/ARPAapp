58b512b7c92fc95a3dcb08a8fb9e413fb0ed6bcc
Mickey253 <jacob_l._miller@hotmail.com>
2019-10-08 16:18:03 -0500

    Updating Algorithm

--- AndroidAppDev/LoadCSV.py
+++ AndroidAppDev/LoadCSV.py
@@ -2,7 +2,11 @@ import csv
 with open('Buildings.csv', newline='') as csvfile:
      spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
      listOfLists = []
-     for row in spamreader:
-         listOfLists.append(row)
-         print(listOfLists)
-        #print(row)
+     counter = 0
+     print(type(listOfLists[0]) == "list")
+     '''for row in spamreader:
+         for x in row:
+             listOfLists[counter].append(x)
+         counter += 1
+     print(listOfLists)
+'''
