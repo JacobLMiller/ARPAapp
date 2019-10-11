33ce1b0cf9190618c2e767a72062c9bf17ce1b1d
Mickey253 <jacob_l._miller@hotmail.com>
2019-10-11 16:57:37 -0500

    Updated Algorithm

--- AndroidAppDev/LoadCSV.py
+++ AndroidAppDev/LoadCSV.py
@@ -1,12 +1,41 @@
 import csv
-with open('Buildings.csv', newline='') as csvfile:
-     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
-     geoData = [[]]
-     counter = 0
-     print(isinstance(geoData, list))
-     for row in spamreader:
-         for x in row:
-             geoData[counter].append(x)
-         counter += 1
-         geoData.append([])
-    # print(listOfLists)
+
+def ConvertCSVToList(fileToOpen):
+    """This method will take any csv file and
+    convert it to a list of lists, with rows being a list
+    with each entry as elements. """
+
+    with open(fileToOpen, newline='') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        geoData = [[]]
+        index = 0
+        #print(isinstance(geoData, list))
+
+        for row in spamreader:
+            for x in row:
+                geoData[index].append(x)
+            index += 1
+            geoData.append([])
+        geoData.pop()
+
+        return geoData
+
+def ListToDict(geoData):
+    geoDataDict = {}
+    indexOfName = -1
+
+    for x in range(len(geoData[0])):
+        if (geoData[0][x] == "name"):
+            indexOfName = x
+
+    for x in range(len(geoData)):
+        pass
+
+    return geoDataDict
+
+def main():
+    fileToOpen = 'Buildings.csv'
+    geoData = ConvertCSVToList(fileToOpen)
+    geoDataDict = ListToDict(geoData)
+
+main()
